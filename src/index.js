9c40ed863e569a109dd528a83c75fde394f0dd97
Matthew Dobson <mdobson4@gmail.com>
2015-02-02 10:24:12 -0500

    Updated for random name generation, or setting server name with env variable.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 var zetta = require('zetta');
 
+function randomName() {
+  var firstNames = ['turbulent', 'tumultuous', 'windy', 'rough', 'calm', 'pristine', 'stormy', 'south', 'north', 'west'];
+  var lastNames = ['huron', 'caribbean', 'michigan', 'erie', 'superior', 'pacific', 'atlantic', 'arctic', 'black_sea', 'red_sea'];  
+
+  var firstName = Math.floor(Math.random() * (firstNames.length));
+  var lastName = Math.floor(Math.random() * (lastNames.length));
+  return [firstNames[firstName], lastNames[lastName]].join('_');
+}
+
+
 zetta()
-  .name('matt.dobson.docker')
+  .name(process.env.NAME || randomName())
   .listen(process.env.NODE_PORT || 1337);
